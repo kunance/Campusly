539aa7be7f63164e23db2af578a8b1ea278ae37a
Andrea Amerigo Aristodemo Gariboldi <andrea.gariboldi@gmail.com>
2014-09-05 16:45:20 +0200

    tenants verify-profile-1

--- angular/src/app/tenant/tenant.js
+++ angular/src/app/tenant/tenant.js
@@ -22,16 +22,35 @@ angular.module('myApp.tenant', ['ngRoute'])
             }
         });
 
-        $routeProvider.when('/tenants/on-boarding', {
+        var TENANTS_ONLY= function (profile)
+            {
+                 console.log('tenants only', profile);
+
+                 if (profile.type!='tenant')
+                   return '/tenants'; 
+            };
+
+        $routeProvider.when('/tenants/on-boarding/:step?', {
             authRequired: '/register/tenant',
             templateUrl: 'tenant/on-boarding.tpl.html',
-            controller: 'OnBoardingCtrl'
+            controller: 'OnBoardingCtrl',
+            profileRequired: TENANTS_ONLY
         });
+
     }
 ])
 
-.controller('OnBoardingCtrl', ['$scope','$rootScope','$location',
-    function($scope,$rootScope,$location) {
+.controller('OnBoardingCtrl', ['$scope','$rootScope','$location','$routeParams',
+    function($scope,$rootScope,$location,$routeParams) {
        $rootScope.secondaryNav= 'tenant/partials/menu-tenant.tpl.html';
+
+       var steps= {
+                     'none':             'tenant/partials/verify-profile-1.tpl.html',
+                     'verify-profile-2': 'tenant/partials/verify-profile-2.tpl.html',
+                     'credit-check':     'tenant/partials/credit-check.tpl.html',
+                     'ready-to-apply':   'tenant/partials/ready-to-apply.tpl.html'
+                  };
+
+       $scope.step= steps[$routeParams.step || 'none'];
     }
 ])
