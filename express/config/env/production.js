3b07333b7a3b28622be65e1e217596bdc9877907
John Raber <jraber@Johns-MacBook-Pro.local>
2014-12-17 19:08:30 -0800

    added work in progress SQL script to create Rented schema and added JS models and seqeulizer ORM code for Node

--- express/config/env/production.js
+++ express/config/env/production.js
@@ -2,16 +2,39 @@
 
 module.exports = {
     env: "production",
-//    ip: process.env.OPENSHIFT_NODEJS_IP ||
-//        process.env.IP ||
-//        "127.0.0.1",  //  change to  0.0.0.0 if not using Nginx in front of Node app
-//    port: process.env.OPENSHIFT_NODEJS_PORT ||
-//        process.env.PORT ||
-//        8080,
-//    mongo: {
-//        uri: process.env.MONGOLAB_URI ||
-//            process.env.MONGOHQ_URL ||
-//            process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME ||
-//            "mongodb://10.9.208.122/rented"
-//    }
+    ip: "0.0.0.0",
+    mysql: {
+        connection: {
+            host: "localhost",
+            port: 3306,
+            user: "rentedNode",
+            password: "rented",
+            database: "rv4",
+            charset: "utf8"
+        }
+    },
+    experian: {
+        protocol: "https",
+        port: 443,
+        ip: "https://stg1-ss6.experian.com",
+        paths: {
+            getOwnReport: "/ECP2P/api/user",
+            submitAuthAnswers: "/ECP2P/api/user/answers",
+            getAuthStatus: "/ECP2P/api/auth/authstatus/",
+            reAuthExistingToken: "/ECP2P/api/auth/",
+            submitAnswersReAuthExistingToken: "/ECP2P/api/auth/answers",
+            getConsumerCreditReport: "/ECP2P/api/report",
+            shareCreditReport: "/ECP2P/api/share",
+            getSharedCreditReport: "/ECP2P/api/share/get",
+            getArchivedConsumerReport: "/ECP2P/api/report/archive"
+
+        },
+        auth: {
+            rented: {
+                username: "connect_rented",
+                password: "cnct1014e"
+            }
+        }
+
+    }
 };
