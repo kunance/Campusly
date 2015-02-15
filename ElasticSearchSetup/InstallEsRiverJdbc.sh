4fa9fd46e9eb246cc3a850f631e48cb07c2e06cf
johnraber <raber.john@gmail.com>
2015-02-15 00:06:35 -0800

    reworked all the Elastic Search install scripts and tested on EC2

--- ElasticSearchSetup/InstallEsRiverJdbc.sh
+++ ElasticSearchSetup/InstallEsRiverJdbc.sh
@@ -8,22 +8,22 @@ sudo unzip mysql-connector-java-5.1.33.zip
 sudo cp mysql-connector-java-5.1.33/mysql-connector-java-5.1.33-bin.jar /usr/share/elasticsearch/plugins/jdbc/
 sudo chmod 644 /usr/share/elasticsearch/plugins/jdbc/*
 
+sudo rm mysql-connector-java-5.1.33.zip
+
 sudo mkdir /usr/share/elasticsearch/config
 
 sudo ln -s /etc/elasticsearch/elasticsearch.yml /usr/share/elasticsearch/config/elasticsearch.yml
 
 
-sudo service elasticsearch stop
+sudo service elasticsearch restart
 
-sudo service elasticsearch start
-
-
-curl -XPUT 'localhost:9200/_river/my_jdbc_river/_meta' -d '{
-    "type" : "jdbc",
-    "jdbc" : {
-        "url" : "jdbc:mysql://172-31-35-208:3306/Rented",
-        "user" : "ivan",
-        "password" : "rentedrented",
-        "sql" : "select * from apartment_complex"
-    }
-}'
+### Make sure service is running ... but service takes a minute to run so don't auto test without a wait first
+#curl -XPUT 'localhost:9200/_river/my_jdbc_river/_meta' -d '{
+#    "type" : "jdbc",
+#    "jdbc" : {
+#        "url" : "jdbc:mysql://172-31-35-208:3306/Rented",
+#        "user" : "ivan",
+#        "password" : "rentedrented",
+#        "sql" : "select * from apartment_complex"
+#    }
+#}'
---
+++ ElasticSearchSetup/removeAllElasticSearch.sh
@@ -0,0 +1,10 @@
+sudo apt-get purge elasticsearch
+sudo update-rc.d elasticsearch remove
+sudo rm -rf /etc/init.d/elasticsearch
+
+# sudo rm -rf /usr/share/elasticsearch/
+# sudo rm -rf /etc/elasticsearch/
+# sudo rm -rf /var/lib/elasticsearch/
+# sudo rm -rf  /etc/default/elasticsearch
+
+
