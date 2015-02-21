fb0beee6d7b2e2d93626edecc825247633e78473
johnraber <raber.john@gmail.com>
2015-02-20 17:06:17 -0800

    removed bug from state change

--- client/app/landing/landing.controller.js
+++ client/app/landing/landing.controller.js
@@ -1,5 +1,18 @@
-angular.module("app.landing").controller("LandingCtrl", function ($scope, $state) {
+(function () {
   "use strict";
 
-});
+  angular
+    .module('app.landing')
+    .controller('LandingCtrl', LandingCtrl);
+
+  LandingCtrl.$inject=['$scope', 'common', '$state'];
+
+  function LandingCtrl($scope, common, $state) {
+
+    // vm for view model .... https://github.com/johnpapa/angularjs-styleguide#style-y032
+    /* jshint validthis: true */
+    var vm = this;
+  }
+
+  }());
 
