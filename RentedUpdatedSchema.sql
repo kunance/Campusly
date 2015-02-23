d14f060c683d440647b2f5739c044d81ea6c5b74
johnraber <raber.john@gmail.com>
2015-02-23 10:58:08 -0800

    removed users table from schema

--- RentedUpdatedSchema.sql
+++ RentedUpdatedSchema.sql
@@ -924,33 +924,6 @@ CREATE TABLE `user_vehicle` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `hashedPassword` varchar(255) DEFAULT NULL,
-  `provider` varchar(255) DEFAULT NULL,
-  `salt` varchar(255) DEFAULT NULL,
-  `facebookUserId` int(11) DEFAULT NULL,
-  `twitterUserId` int(11) DEFAULT NULL,
-  `twitterKey` varchar(255) DEFAULT NULL,
-  `twitterSecret` varchar(255) DEFAULT NULL,
-  `github` varchar(255) DEFAULT NULL,
-  `openId` varchar(255) DEFAULT NULL,
-  `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping events for database 'rented'
 --
 
