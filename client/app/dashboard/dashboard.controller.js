318df5f1f8c7cbd89d5cd012ff42d43926475342
jmscarpa <joao.scarpa@gmail.com>
2015-02-24 22:04:27 -0300

    Slider And three main boxes of dashboard

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -3,24 +3,24 @@
   "use strict";
 
   angular
-    .module('app.dashboard')
-    .controller('DashboardCtrl',DashboardCtrl);
+  .module('app.dashboard')
+  .controller('DashboardCtrl',DashboardCtrl);
 
-  DashboardCtrl.$inject=['common', '$scope'];
+  DashboardCtrl.$inject=['common', '$scope', '$http'];
 
   function DashboardCtrl(common, $scope) {
     var vm = this;
     var Auth = common.Auth;
-    vm.address = {};
+    vm.address = { };
     vm.me = Auth.getCurrentUser();
 
     vm.tabs = [
-      { title:"Dashboard", sref:"dashboard", active: true },
-      { title:"My profile", sref:".myProfile"},
-      { title:"My properties", sref:".myProperties"},
-      { title:"My connections", sref:".myConnections"},
-      { title:"My favorites", sref:".myFavorites"},
-      { title:"Messages", sref:".messages"}];
+    { title:"Dashboard", sref:"dashboard", active: true },
+    { title:"My profile", sref:".myProfile"},
+    { title:"My properties", sref:".myProperties"},
+    { title:"My connections", sref:".myConnections"},
+    { title:"My favorites", sref:".myFavorites"},
+    { title:"Messages", sref:".messages"}];
 
     mixpanel.identify(vm.me.id);
     mixpanel.people.set({
@@ -31,11 +31,75 @@
       "$phone": vm.me.phone,
       "$last_login": new Date()
     });
+
     $scope.$parent.seo = {
       pageTitle:'Welcome to Rented co',
       pageDescription:'Beast and easiest way to rent a place'
     };
+
     mixpanel.track("visited dashboard view, with passed object",{title:$scope.$parent.seo.pageTitle});
+
+    vm.breakpoints = {
+      availableRooms: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }, {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+      , {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      ],
+       lookingRoom: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }, {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      ],
+       aroundYou: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5
+        }
+      }, {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+      ]
+    }
+
   }
 
 }());
