714d4b45792d74eec0ec8c293014377565bf2104
Ivan Mornar <ivan.mornar@toptal.com>
2015-03-10 13:37:55 +0100

    adding get all rooms to resource, and route resolve

--- client/app/rooms/rooms.controller.js
+++ client/app/rooms/rooms.controller.js
@@ -5,25 +5,20 @@
     .module('app.rooms')
     .controller('RoomsCtrl', RoomsCtrl);
 
-  RoomsCtrl.$inject = ['$scope', 'common', 'FileUploader', '$http', 'RoomListingView'];
+  RoomsCtrl.$inject = ['$scope', 'common', 'FileUploader', '$http', 'getAllRooms'];
 
-  function RoomsCtrl($scope, common, FileUploader, $http, RoomListingView) {
+  function RoomsCtrl($scope, common, FileUploader, $http, getAllRooms) {
     var vm = this;
     vm.property = {};
     vm.me = common.Auth.getCurrentUser();
+    vm.availableRooms = getAllRooms;
+
+    console.log(vm.availableRooms);
 
     //vm.availableRooms = RoomListingView.query(function(/*availRooms*/) {
      // vm.availableRooms = availRooms;
     //  console.log("availableRooms: ", vm.availableRooms);
     //});
-
-    $http.get('api/rooms').success(function (rooms) {
-      console.log('rooms',rooms);
-      vm.availableRooms = rooms;
-    })
-
-     console.log("availableRooms: ", vm.availableRooms);
-
 
     //$http.get("../assets/fake/available_rooms.json")
     //  .success(function(data){
