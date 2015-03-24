7d2d8886bd69acb77c312c5141c4c5563e84c344
Vinit Modi <vinitmodi@Vinits-MacBook-Pro.local>
2015-03-24 09:55:03 -0700

    Dashboard roommate message CSS. Updated insert scripts. Fixed alignment issue on Sort box

--- database_scripts/InsertRoommate.sql
+++ database_scripts/InsertRoommate.sql
@@ -1,17 +1,17 @@
-INSERT INTO roommate ("id", "userId", "roommateId")
+INSERT INTO roommate ("id", "userId", "roommateId", "confirmed")
 VALUES
-(2,2,4),
-(3,4,2),
-(4,5,6),
-(5,5,7),
-(6,6,5),
-(7,6,7),
-(8,7,5),
-(9,7,6),
-(10,10,17),
-(11,17,10),
-(12,14,15),
-(13,15,14);
+(2,2,4,'true'),
+(3,4,2,'true'),
+(4,5,6,'true'),
+(5,5,7,'true'),
+(6,6,5,'true'),
+(7,6,7,'true'),
+(8,7,5,'true'),
+(9,7,6,'true'),
+(10,10,17,'true'),
+(11,17,10,'true'),
+(12,14,15,'true'),
+(13,15,14,'true');
 
 --
 -- TOC entry 2681 (class 0 OID 0)
