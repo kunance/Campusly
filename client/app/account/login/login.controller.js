749c9dc8bb1dc262cb78e660c0c2d0710f620496
Ivan <ivan.mornar@toptal.com>
2015-03-30 14:01:10 +0200

    adding mail verification front end error handling, feedback to user if failed. Adding signing in indicator

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -11,6 +11,7 @@
     if(currentUser.confirmedEmail===false){
       common.Auth.logout();
     }
+    $scope.showForm = true;
     $scope.user = {};
     $scope.reset = '';
     $scope.errors = {};
@@ -18,7 +19,6 @@
     $scope.newPasswordAddon = false;
     $scope.reset = $scope.newPasswordAddon ? 'I remember!' : 'Forget password?';
     $scope.confirmToken = $stateParams.confirmToken;
-    $scope.invalidToken = false;
     var passwordResetToken = $stateParams.passwordResetToken;
     $scope.pwdResetMailSend = false;
 
@@ -41,7 +41,8 @@
           $state.go('login');
         })
         .catch( function() {
-          $scope.invalidToken = true;
+          $scope.$parent.invalidToken = true;
+          $state.go('login');
         });
     }
 
@@ -61,7 +62,10 @@
           .then(function () {
             common.Auth.getCurrentUser(function (user) {
               if (user.confirmedEmail) {
-                $state.go('dashboard');
+                  $scope.loading = true;
+                  $scope.success = false;
+                  $scope.showForm = false;
+                  $state.go('dashboard');
               } else {
                 $scope.showResendPartial = true;
                 $scope.errors = {verifiedEmail: user.firstname+', your e-mail address is not verified. Please verify your Campusly account.'};
@@ -99,6 +103,7 @@
     $scope.loginOauth = function (provider) {
       $window.location.href = '/auth/' + provider;
     };
+
   }
 }());
 
