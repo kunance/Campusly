413d8046716baf92afdd83ddef9b2a7365fb3d61
Vinit Modi <vinitmodi@Vinits-MacBook-Pro.local>
2015-03-31 11:56:51 -0700

    Spinner path fix. Removed unused file

--- client/components/widgets/my-spinner/my.spinner.js
+++ client/components/widgets/my-spinner/my.spinner.js
@@ -17,7 +17,7 @@
       link: function(scope, elem, attrs) {
         //console.log(scope.title);
       },
-      templateUrl: '../../../components/widgets/my-spinner/my.spinner.html'
+      templateUrl: 'components/widgets/my-spinner/my.spinner.html'
     };
   }
 }());
