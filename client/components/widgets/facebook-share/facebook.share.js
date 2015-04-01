6a186b8d01e65238adece70c3f4469c233020d5b
Ivan <ivan.mornar@toptal.com>
2015-04-01 16:06:23 +0200

    facebook share like

---
+++ client/components/widgets/facebook-share/facebook.share.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.widgets')
+    .directive('fbLike', [
+      '$window', '$rootScope', function ($window, $rootScope) {
+        return {
+          restrict: 'A',
+          scope: {
+            fbLike: '=?'
+          },
+          link: function (scope, element, attrs) {
+            if (!$window.FB) {
+              // Load Facebook SDK if not already loaded
+              $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
+                $window.FB.init({
+                  appId: 1482591365325802,
+                  xfbml: true,
+                  version: 'v2.3',
+                  picture:'http://i61.tinypic.com/2dj1e84.jpg'
+                });
+                renderLikeButton();
+              });
+            } else {
+              renderLikeButton();
+            }
+
+            var watchAdded = false;
+            function renderLikeButton() {
+              if (!!attrs.fbLike && !scope.fbLike && !watchAdded) {
+                // wait for data if it hasn't loaded yet
+                var watchAdded = true;
+                var unbindWatch = scope.$watch('fbLike', function (newValue, oldValue) {
+                  if (newValue) {
+                    renderLikeButton();
+                    // only need to run once
+                    unbindWatch();
+                  }
+                });
+                return;
+              } else {
+                //change class to fb-share in order to support multiple friends
+                element.html('<div class="fb-like"' + (!!scope.fbLike ? ' data-href="' + scope.fbLike + '"' : '') + ' data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>');
+                $window.FB.XFBML.parse(element.parent()[0]);
+              }
+            }
+          }
+        };
+      }
+    ]);
+
+}());
