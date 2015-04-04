1e3fb231a0974d482dc7bd77711dec7bd3215814
Ivan Mornar <ivan.mornar@toptal.com>
2015-04-04 23:32:57 +0200

    adding lookings template

--- server/components/mail/user_update/index.js
+++ server/components/mail/user_update/index.js
@@ -1,108 +1,83 @@
-var sqldb = require('../../../sqldb');
-var User = sqldb.model('rentedUser');
-var RoomListing = sqldb.model('roomListing');
+'use strict';
+
 var config = require('../../../config/environment');
-var Looking = sqldb.model('looking');
-var mandrill = require('node-mandrill')(config.mandrill.APIkey);
-var schedule = require('node-schedule');
+var mailService = require('../../mail/mail.service');
 
-module.exports.UserEvery24h = function (req, res, next) {
+var sendMail = function(User, RoomListing, Looking, Education, callback){
 
-/*
- * option: j.cancel();
- * starts: [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
- */
+  var current = new Date();
+  var minus24hours = new Date(current);
+  minus24hours.setDate(current.getDate()-1);
 
-  //var j = schedule.scheduleJob('4 00 * * *', function(){
-  //
-  //  var lookingCount = 0;
-  //  var roomListingCount = 0;
-  //  var recipients = null;
-  //  var d = new Date();
-  //
-  //  recipients = {
-  //    message: {
-  //      from_email: config.mail.from_email,
-  //      from_name: config.mail.from_name,
-  //      "to":[],
-  //      subject: 'update'
-  //    }
-  //  };
-  //
-  //  User.findAll({})
-  //    .then(function (users) {
-  //      if(users) {
-  //        for (var i = 0; i < 2; i += 1) {
-  //          recipients.message.to.push({
-  //            email: users[i].email,
-  //            name: users[i].firstname + ' ' + users[i].lastname,
-  //            type: 'to'
-  //          })
-  //        }
-  //      } else {
-  //        console.log('canceled');
-  //        j.cancel();
-  //      }
-  //    Looking.findAll({where:
-  //    {
-  //      createdAt: {gte: d}
-  //    }})
-  //      .then(function (lookings) {
-  //        lookingCount = lookings.length;
-  //        RoomListing.findAll({where:
-  //        {
-  //          createdAt: {gte: d}
-  //        }})
-  //          .then(function (roomListings) {
-  //            roomListingCount = roomListings.length;
-  //
-  //            if(roomListingCount>0 || lookingCount>0) {
-  //              recipients.message.text = "Hello, today is published " + roomListingCount + " new rooms, and " + lookingCount + " posted looking. More info on http://www.campusly.org";
-  //              mandrill('/messages/send', recipients, function (error, info) {
-  //                if (error) {
-  //                  console.log(error);
-  //                }
-  //                else {
-  //                  console.log(info);
-  //                }
-  //              });
-  //            } else {
-  //              console.log('canceled');
-  //              j.cancel();
-  //            }
-  //          })
-  //          .catch(function (errors) {
-  //            console.log(errors);
-  //            res.status(500).json(errors);
-  //          });
-  //        })
-  //        .catch(function (errors) {
-  //          console.log(errors);
-  //          res.status(500).json(errors);
-  //        });
-  //      })
-  //      .catch(function (errors) {
-  //        console.log(errors);
-  //        res.status(500).json(errors);
-  //      });
-  //});
+  var locals = {
+  lookings : 0,
+  roomListings : 0,
+  recipients : [],
+  d : minus24hours
+  };
+
+  locals.recipients = {
+    message: {
+      from_email: config.mail.from_email,
+      from_name: config.mail.from_name,
+      "to":[],
+      subject: 'daily update'
+    }
+  };
+  var userAttributes = ['firstname'];
+  User.findAll({})
+    .then(function (users) {
+      if(users) {
+        for (var i = 0; i < users.length; i += 1) {
+          locals.recipients.message.to.push({
+            email: users[i].email,
+            name: users[i].firstname + ' ' + users[i].lastname,
+            type: 'to'
+          })
+        }
+      } else {
+        console.log('canceled1');
+      }
+      Looking.findAll({where:
+      {
+        activeLooking:true,
+        createdAt: {gte: locals.d}
+      },
+        include: [
+          { model: User, attributes: userAttributes, as: 'relatedUserId',
+          include:[
+        { model: Education, as: 'usereducationUsers'}]}
+        ]
+      })
+        .then(function (lookings) {
+          if(lookings) locals.lookings = lookings;
+          RoomListing.findAll({where:
+          {
+            activeRoom:true,
+            createdAt: {gte: locals.d}
+          }})
+            .then(function (roomListings) {
+              if(roomListings) locals.roomListings = roomListings;
+              if(locals.lookings.length>0 || locals.roomListings.length>0) {
+
+                mailService.updateUsers('user_update', null, 'Campusly - daily update', locals, callback);
+
+              } else {
+                console.log('canceled2');
+              }
+            })
+            .catch(function (errors) {
+              console.log(errors);
+            });
+        })
+        .catch(function (errors) {
+          console.log(errors);
+        });
+    })
+    .catch(function (errors) {
+      console.log(errors);
+    });
 
 };
 
-//"to":[
-//  {
-//    "email":"recipient.email@example.com",
-//    "name":"Recipient Name",
-//    "type":"to"
-//  },
-//  {
-//    "email":"bcc.recipient.email.1@example.com",
-//    "name":"BCC Recipient Name 1",
-//    "type":"bcc"
-//  },
-//  {
-//    "email":"bcc.recipient.email.2@example.com",
-//    "name":"BCC Recipient Name 2",
-//    "type":"bcc"
-//  }
-//]
+exports.sendMail = sendMail;
