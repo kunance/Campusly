7e2354c191b3bbf66b86f84580f5ccd7b6ce91c5
Ivan <ivan.mornar@toptal.com>
2015-04-15 09:38:57 +0200

    prepull

---
+++ client/app/aroundYou/around.you.controller.js
@@ -0,0 +1,51 @@
+(function () {
+  "use strict";
+
+  angular
+    .module('app.dashboard')
+    .controller('aroundYouCtrl', aroundYouCtrl);
+
+  aroundYouCtrl.$inject = ['common', 'currentUser', 'data', 'UserResource', '$window'];
+
+  function aroundYouCtrl(common, currentUser, data, UserResource, $window) {
+    var vm = this;
+    vm.me = currentUser;
+    vm.education = data[0];
+    var currentUniversityId = vm.education.universityId;
+
+    UserResource.aroundMe({distance:( 5 * 1609 ), limit: 20}, function (aroundYou) {
+      vm.aroundYou = aroundYou;
+      vm.groups = vm.aroundYou.inGroupsOf(8);
+    }, function (err) {
+      common.logger.error('something went wrong!');
+    });
+
+
+
+
+    function orderSliderButtons() {
+      setTimeout(function() {
+        $(".slider").each(function(index) {
+          var slider = $(".slider").eq(index);
+          var dotsX = parseInt(slider.find(".slick-dots").css("left"));
+          var dotsSize = parseInt(slider.find(".slick-dots").css("width"));
+          var nextBtnX = dotsX + dotsSize + 10;
+
+          slider.find(".slick-next").css("left", nextBtnX);
+        });
+      }, 1000);
+    }
+
+    angular.element($window).bind('resize', function () {
+      orderSliderButtons();
+    });
+
+    angular.element(document).ready(function () {
+      orderSliderButtons();
+    });
+
+
+    mixpanel.track('aroundYou grid view');
+  }
+
+}());
