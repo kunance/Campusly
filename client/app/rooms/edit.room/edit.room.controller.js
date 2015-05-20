645d74d42d999d7bcfa9cf8b268013eaac4d6805
Vinit Modi <vinitmodi@Vinits-MacBook-Pro.local>
2015-05-20 03:08:29 -0700

    Removed add roommate code from controller. Clicking Save on Edit Room removed ng-disabled pristine

--- client/app/rooms/edit.room/edit.room.controller.js
+++ client/app/rooms/edit.room/edit.room.controller.js
@@ -106,64 +106,6 @@
       $scope.datePickers[number]= true;
     };
 
-    vm.showNewRoomate = false;
-    vm.showRoommatesAddonButtons = false;
-
-    vm.toggleAddRoommate = function(){
-      vm.showAddRoommate = true;
-      vm.showInviteRoomate = false;
-      vm.showRoommatesAddonButtons = true;
-    };
-
-    vm.toggleInviteRoomate = function(){
-      vm.showAddRoommate = false;
-      vm.showInviteRoomate = true;
-      vm.showRoommatesAddonButtons = true;
-    };
-
-    vm.toggleRoommatesAddon = function(){
-      vm.showRoommatesAddonButtons = !vm.showRoommatesAddonButtons;
-      vm.showAddRoommate = false;
-      vm.showInviteRoomate = false;
-    };
-
-    vm.cancelRoommateAddAddon = function (){
-      vm.showRoommateAddonButtons = false;
-      vm.showAddRoommate = false;
-      vm.showInviteRoommate = false;
-    };
-
-    vm.cancelRoommateInviteAddon = function (){
-      vm.showRoommateAddonButtons = false;
-      vm.showAddRoommate = false;
-      vm.showInviteRoomate = false;
-    };
-
-    vm.removeRoommate= function (roommate) {
-      var index= vm.roommates.indexOf(roommate);
-      var id = roommate.id;
-      common. dataservice.deleteRoommate(vm.me.id, id, function () {
-        vm.roommates.splice(index, 1);
-        common.logger.success('Successfully removed roommate');
-      })
-    };
-
-   $scope.addNewRoommate = function(input){
-      if(!input) { return false; }
-      input.originalObject.roommateId = input.originalObject.id;
-      var roommate = input.originalObject;
-      common.dataservice.addRoommate(vm.me.id, roommate)
-        .$promise
-        .then(function (data) {
-          common.logger.success('Roommate successfully added!');
-          common.$state.reload();
-          vm.roommates.push(data);
-        })
-        .catch(function (err) {
-          common.logger.error('Something went wrong. Roommate not saved.');
-        });
-    };
-
     mixpanel.track("edit your room");
     mixpanel.people.increment('edit a room');
 
