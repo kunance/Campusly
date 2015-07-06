1654b8e399569bff60fe61b5bdb5d86ced3e8d40
Vinit Modi <vinitmodi@Vinits-MacBook-Pro.local>
2015-07-06 11:41:41 -0700

    Updated structure for Pubnub

--- client/app/messages/messages.controller.js
+++ client/app/messages/messages.controller.js
@@ -6,21 +6,37 @@
   .module('app.messages')
   .controller('MessageCtrl',MessageCtrl);
 
-  MessageCtrl.$inject= ['common', '$scope', 'currentUser', 'UserResource', '$q'];
+  MessageCtrl.$inject= ['common', '$scope', 'currentUser', 'UserResource', '$q', 'PubNub'];
 
-  function MessageCtrl(common, $scope, currentUser, UserResource, $q) {
+  function MessageCtrl(common, $scope, currentUser, UserResource, $q, PubNub) {
     var vm = this;
     vm.me = currentUser;
 
     /*
-     *  prerender.io
+     * PubNub init code
+     * Keys used are from Sandbox - TODO - change before launching
+     */
+    PubNub.init({
+      publish_key:'pub-c-cd12098a-7ff3-4558-921b-c4c7a70ed47a',
+      subscribe_key:'sub-c-fb61f4f0-2402-11e5-8463-02ee2ddab7fe',
+      uuid: vm.me //use existing user's ID as the UUID - unique user ID
+    });
+
+    /*
+     * Start first chat code here
+     */
+
+    /*
+     *  prerender.io for SEO
      */
     $scope.$parent.seo = {
       pageTitle: 'Messages',
       pageDescription: 'Messages'
     };
 
-
+    /*
+     *  Mixpanel tracking code
+     */
     mixpanel.track("messages");
     mixpanel.people.increment('messages');
 
--- client/app/messages/messages.module.js
+++ client/app/messages/messages.module.js
@@ -3,7 +3,7 @@
   "use strict";
 
   angular
-    .module('app.messages', [])
+    .module('app.messages', ['pubnub.angular.service'])
     .config(config);
 
      config.$inject = ['$stateProvider'];
