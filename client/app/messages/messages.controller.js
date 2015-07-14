1366e16766cac2aa8c9ff8fc078f6d455df8d989
Vinit Modi <vinitmodi@Vinits-MacBook-Pro.local>
2015-07-13 23:39:55 -0700

    Added channels and initializing function to take care of promises

--- client/app/messages/messages.controller.js
+++ client/app/messages/messages.controller.js
@@ -10,103 +10,121 @@
 
   function MessageCtrl(common, $scope, currentUser, UserResource, $q, PubNub) {
     var vm = this;
-    var retrieved = 0;
-    var shortName;
     vm.me = currentUser;
     vm.education = common.dataservice.getAllEducations(currentUser.id);
-    
-    vm.education.$promise.then(function(data){
-       console.log(data);
+
+    /*
+     * Collect all the promises in an array and load the initialize function only if the promises are met
+     */
+    var promises = [vm.education.$promise];
+    $q.all(promises).then(function () {
+      initializeMessageController()
     });
 
+    function initializeMessageController () {
 
-    var email = JSON.stringify(vm.me.email);
-    console.log(email);
-
-    var messages = [];"User " + Math.round(Math.random() * 1000);
-
-    // make up a user id (you probably already have this)
-    vm.userId   =
-    // set up initial channel memberships
-    vm.subscriptions = { channel1 : true, channel2 : true };
-    // pre-populate any existing messages (just an AngularJS scope object)
-    vm.messages = messages;
-
-
-    PubNub.init({
-      subscribe_key: 'sub-c-fb61f4f0-2402-11e5-8463-02ee2ddab7fe',
-      publish_key: 'pub-c-cd12098a-7ff3-4558-921b-c4c7a70ed47a',
-      uuid: email
-    });
-
-    vm.publishMessage = function(message){
-      messages.push(message);
-      //console.log(messages);
+      var email = JSON.stringify(vm.me.email);
+      var messages = [];"User " + Math.round(Math.random() * 1000);
       vm.messages = messages;
-      console.log(vm.messages);
-    };
 
-    // Create a function to subscribe to a channel
-    vm.subscribe = function(theChannel) {
-      // Subscribe to the channel via PubNub
-      PubNub.ngSubscribe({ channel: theChannel });
-      console.log("subscribe");
+      /*
+       * Identify list of default channels
+       * Format: University + Channel Name
+       */
+      var universityChannel = JSON.stringify("#" + vm.education.relatedUniversityId.shortName);
+      var careerCenterChannel = JSON.stringify("#" + vm.education.relatedUniversityId.shortName + " Career Center");
+      var resLifeChannel = JSON.stringify("#" + vm.education.relatedUniversityId.shortName + " ResLife");
+      var academicAdvisingChannel = JSON.stringify("#" + vm.education.relatedUniversityId.shortName + " Academic Advising");
+      var finAidChannel = JSON.stringify("#" + vm.education.relatedUniversityId.shortName + " Financial Aid");
 
-      // Register for message events
-      $scope.$on(PubNub.ngMsgEv(theChannel), function(ngEvent, payload) {
-        $scope.$apply(function() {
-          vm.publishMessage(payload.message);
+      //console.log (universityChannel);
+
+      vm.subsciptions = { universityChannel : true, careerCenterChannel : true, resLifeChannel : true, academicAdvisingChannel : true,  finAidChannel : true };
+
+      /*
+       * PubNub init code
+       * Keys used are from Sandbox - TODO - change before launching
+       */
+      PubNub.init({
+        publish_key:'pub-c-cd12098a-7ff3-4558-921b-c4c7a70ed47a',
+        subscribe_key:'sub-c-fb61f4f0-2402-11e5-8463-02ee2ddab7fe',
+        uuid: email, //use existing user's ID as the UUID - unique user ID
+        ssl: true
+      });
+
+      vm.publishMessage = function(message){
+        messages.push(message);
+        //console.log(messages);
+        vm.messages = messages;
+        console.log(vm.messages);
+      };
+
+      // Create a function to subscribe to a channel
+      vm.subscribe = function(theChannel) {
+        // Subscribe to the channel via PubNub
+        PubNub.ngSubscribe({ channel: theChannel });
+        console.log("subscribe");
+
+        // Register for message events
+        $scope.$on(PubNub.ngMsgEv(theChannel), function(ngEvent, payload) {
+          $scope.$apply(function() {
+            vm.publishMessage(payload.message);
           });
-      });
-    };
+        });
+      };
 
 
-    console.log(messages);
+      console.log(messages);
 
-    // Create a function to unsubscribe from a channel
-    vm.unsubscribe = function(theChannel) {
-      // Unsubscribe from the channel via PubNub
-      PubNub.ngUnsubscribe({ channel: theChannel });
-    };
-    // Create a publish() function in the scope
-    vm.publish = function(channel) {
-      PubNub.ngPublish({
-        channel: channel,
-        message: vm.me.firstname + " " + vm.me.lastname[0] + " @" + channel + "]: " + vm.newMessage
-      });
-      vm.newMessage = '';
-    };
+      // Create a function to unsubscribe from a channel
+      vm.unsubscribe = function(theChannel) {
+        // Unsubscribe from the channel via PubNub
+        PubNub.ngUnsubscribe({ channel: theChannel });
+      };
+      // Create a publish() function in the scope
+      vm.publish = function(channel) {
+        PubNub.ngPublish({
+          channel: channel,
+          message: vm.me.firstname + " " + vm.me.lastname[0] + " @" + channel + "]: " + vm.newMessage
+        });
+        vm.newMessage = '';
+      };
 
-    //$scope.
+      //$scope.
 
-    // Create a subscribe/unsubscribe click handler
-    vm.updateSubscription = function(theChannel) {
-      if (vm.subscriptions[theChannel]) {
-        vm.subscribe(theChannel);
-        console.log(theChannel);
-      } else {
-        vm.unsubscribe(theChannel);
-        console.log(theChannel + " unsubscribed!");
-      }
-    };
+      // Create a subscribe/unsubscribe click handler
+      vm.updateSubscription = function(theChannel) {
+        if (vm.subscriptions[theChannel]) {
+          vm.subscribe(theChannel);
+          console.log(theChannel);
+        } else {
+          vm.unsubscribe(theChannel);
+          console.log(theChannel + " unsubscribed!");
+        }
+      };
+
+      /*
+       *  prerender.io for SEO
+       */
+      $scope.$parent.seo = {
+        pageTitle: 'Messages',
+        pageDescription: 'Messages'
+      };
+
+      /*
+       *  Mixpanel tracking code
+       */
+      mixpanel.track("messages");
+      mixpanel.people.increment('messages');
+    }
+
+    ///// Code that may or may not be needed
+    var retrieved = 0;
 
     // Set up the initial channel subscriptions
     //vm.updateSubscription('channel1');
     //$scope.updateSubscription('channel2');
 
-    /*
-     *  prerender.io for SEO
-     */
-    $scope.$parent.seo = {
-      pageTitle: 'Messages',
-      pageDescription: 'Messages'
-    };
-
-    /*
-     *  Mixpanel tracking code
-     */
-    mixpanel.track("messages");
-    mixpanel.people.increment('messages');
 
   }
 
