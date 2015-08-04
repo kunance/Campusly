298f3132ce3bd348f2c818157618429f24de32c4
Vinit Modi <vinitmodi@Vinits-MacBook-Pro.local>
2015-08-04 15:16:42 -0700

    v2 chat html with CSS

--- client/app/messages/messages.controller.js
+++ client/app/messages/messages.controller.js
@@ -340,6 +340,14 @@
          1. calls currentSubscribe passing in only the name as first param and second param as null
        */
       vm.groupChannelCurrentSubscribe = function(channelName){
+        $scope.setSelected = function() {
+          if ($scope.lastSelected) {
+            $scope.lastSelected.selected = '';
+          }
+          this.selected = 'selected';
+          $scope.lastSelected = this;
+        };
+
         vm.currentSubscribe(channelName, null);
       };
 
@@ -416,7 +424,7 @@
 
         PubNub.jsapi.history({
           channel: channelName,
-          count: 2,
+          count: 100,
           reverse: false,
           include_token: true,
           callback: function(m){
@@ -563,12 +571,15 @@
          and add to the beginning of the currentMessages array. To stay in chronological order
        */
 
+      //initialize all the group channels
       vm.groupChannelInitialization();
-      vm.currentSubscribe('testMe', 'testMe2');
-      console.log(vm.groupChannels);
-      vm.privateChannelHashCode('aayang@ucsd.edu', 'asdf@ucsd.edu');
-      vm.privateSubscribe(vm.me.email);
 
+      //subscribe user to default university channel upon clicking messages in the navbar
+      vm.groupChannelCurrentSubscribe(vm.groupChannels[0].name);
+
+      console.log(vm.groupChannels);
+      //subscribe to one's inbox
+      vm.privateSubscribe(vm.me.email);
 
     }
   };
